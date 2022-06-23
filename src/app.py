e7025d812b8b42b7270b7f7550226f9f1b9bf919
InTr0p <christianolivare01@gmail.com>
2022-06-23 14:12:27 +0000

    Returning json

--- src/app.py
+++ src/app.py
@@ -1,9 +1,17 @@
-from flask import Flask 
+from flask import Flask, jsonify
 app = Flask (__name__)
+import json
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    return '<h1>Hello!</h1>'
+    json_text = jsonify(todos)
+    return json_text
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
