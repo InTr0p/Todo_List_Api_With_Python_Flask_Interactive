8c1959e973dbb2c2215c8f39964f507ba423aaf6
InTr0p <christianolivare01@gmail.com>
2022-06-23 14:50:27 +0000

    Finish_'/todos'

--- src/app.py
+++ src/app.py
@@ -21,8 +21,10 @@ def add_new_todo():
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
+    todos.pop(2)
     print("This is the position to delete: ",position)
-    return 'something'
+    return jsonify(todos)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
