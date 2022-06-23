34a2f05e77a24762b89bf18af5f2e1bb22eca4c9
InTr0p <christianolivare01@gmail.com>
2022-06-23 14:36:40 +0000

    Finish_POST_'/todos'

--- src/app.py
+++ src/app.py
@@ -13,9 +13,13 @@ def hello_world():
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body = request.data
-    print('Incoming request with the following body', request_body)
-    return 'Response for the POST todo'
+    request_body = request.get_json(force=True)
+    todos.append(request_body)
+    print("Incoming request with the following body", request_body)
+    json_text2 = jsonify('Response for the POST todo')
+    return jsonify(todos)
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
